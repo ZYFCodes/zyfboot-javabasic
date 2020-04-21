1b479bd624172515cfc063ec631fce9595b16603
zhangyanfeng <zhangyanfeng@mafengwo.com>
2020-04-21 10:35:19 +0800

    单例模式调整

--- src/main/java/org/zyf/javabasic/designpatterns/singleton/HungrySingleton.java
+++ src/main/java/org/zyf/javabasic/designpatterns/singleton/HungrySingleton.java
@@ -1,9 +1,7 @@
 package org.zyf.javabasic.designpatterns.singleton;
 
 /**
- * 描述：饿汉式单例模式    ====J2EE中的ServlertContext、serletContextConfig
- * ====Spring框架应用中的ApplicationContext
- * ====数据库连接池
+ * 描述：饿汉式单例模式
  * 优点：没有任何锁，执行效率高，用户体验比懒汉式单例模式更好
  * 缺点：类加载的时候就初始化，不管用不用都占内存空间
  * 建议：适用于单例模式较少的场景
